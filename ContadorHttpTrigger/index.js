17a87433b02b7ef48e8e9319f0132762fd0cf063
vinbeze <vinicius_bezerra@hotmail.com>
2022-06-05 15:39:53 -0300

    feat: adicionado id no endpoint

--- ContadorHttpTrigger/index.js
+++ ContadorHttpTrigger/index.js
@@ -3,29 +3,48 @@ var db = require("../lib/mongodb.js");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    const id = context.bindingData.id;
+
+
     db.init();
     
 
-    let itens = await db.findItemsByIdEstabelecimentoAndToday(2543);
+    let itens = await db.findItemsByIdEstabelecimentoAndToday(id);
 
-    const total = itens.map((item) => item.valor ).reduce((previousValue, currentValue) => previousValue + currentValue, 0.00);
+    let response = "";
 
-    const item = itens[0];
-    let hoje = new Date();
-    hoje.setUTCHours(0,0,0,0);
+    if(itens.length > 0)
+    {
+        const total = itens.map((item) => item.valor ).reduce((previousValue, currentValue) => previousValue + currentValue, 0.00);
+
+        const item = itens[0];
+        let hoje = new Date();
+        hoje.setUTCHours(0,0,0,0);
+    
+    
+        
+        response = {
+            id_estabelecimento: item.id_estabelecimento,
+            nome_estabelecimento: item.nome_estabelecimento,
+            data: hoje.toISOString(),
+            valor: total > 0 ? total : 0.00
+        }
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: response
+        };
 
 
-    const response = {
-        id_estabelecimento: item.id_estabelecimento,
-        nome_estabelecimento: item.nome_estabelecimento,
-        data: hoje.toISOString(),
-        valor: total
+    }else {
+        context.res = {
+            status: 404, 
+            body: "estabelecimento não encontrado"
+        };
     }
 
 
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: response
-    };
+
+
+
 }
