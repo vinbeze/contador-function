09c19011099880091bc3b66115672adefeccefb7
vinbeze <vinicius_bezerra@hotmail.com>
2022-06-05 16:13:30 -0300

    feat: descartado valores em zero

--- ContadorServiceBusTrigger/index.js
+++ ContadorServiceBusTrigger/index.js
@@ -6,8 +6,15 @@ module.exports = async function(context, mySbMsg) {
 
     const mensagem = mySbMsg;
     db.init();
+
+    if(mySbMsg.valor > 0){
+        db.addItem(mySbMsg)
+    }
+    else{
+        context.log('Mensagem não salva: ', mySbMsg);
+    }
+
     
-    db.addItem(mySbMsg)
 
     context.log('Mensagem salva: ', mySbMsg);
 };
